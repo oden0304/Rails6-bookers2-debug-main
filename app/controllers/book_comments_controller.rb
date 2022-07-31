34ea32426f4e038780bbadd03511b11541ea911d
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-07-31 05:05:51 +0000

    [応用課題３おわり]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,10 +2,17 @@ class BookCommentsController < ApplicationController
   
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comment.new(book_comment_params)
+    comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
     redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.find_by(book_id: book.id)
+    comment.destroy
+    redirect_to request.referer
   end
   
   private
