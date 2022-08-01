f4ce8864e675f3f5a5385bb458cf7b684b767ab9
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-08-01 08:21:58 +0000

    [応用課題４]モデル、コントローラーの作成、記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites,dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+  
+  has_many :reverce_of_relationships, class_name: 'relationships', foreign_key: "followerd_id", dependent: :destroy
+  has_many :followers, through: :reverce_of_relationships,source: :following
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -15,5 +20,19 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  #relationships_controllerで使うメソッド↓
+  
+  def follow(user_id) #フォローした時の処理
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id) #フォローを外すときの処置
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user) #フォローしているかの判定
+    followings.include?(user)
   end
 end
