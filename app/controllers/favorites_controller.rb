437a4e24bb6b7fa3312d097d6b5edbe0a8206aaa
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-08-03 02:32:56 +0000

    [応用課題]いいね機能非同期通信作成（エラーあり）

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,13 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to request.referer
+    #非同期通信のためredirect_toは削除
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
+    #非同期通信のためredirect_toは削除
   end
 end
