29ffb14c1db0575f2f698f016d3624ed715ce531
oden0304 <108508043+oden0304@users.noreply.github.com>
2022-08-04 06:00:15 +0000

    [応用課題６]コメント機能　非同期通信追加（エラーあり）

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,15 @@
 class FavoritesController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     #非同期通信のためredirect_toは削除
   end
   
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     #非同期通信のためredirect_toは削除
   end
